d5ea46be3174020f20a9823dadfefe2a59850e0a
No-Lag-PH <182519863+Scripter2k25@users.noreply.github.com>
2025-08-20 20:04:09 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,4 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua"))()
-
 ID="6d248b0f-13a3-4922-8a49-254c503501fc";
 loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
